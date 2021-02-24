d112ca465441d9f1ea747798398d549a83126d54
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-02-24 15:45:45 -0500

    Return outputs of load balancer and target group

--- datacenter/outputs.tf
+++ datacenter/outputs.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "listener_arn" {
+  value = aws_lb_listener.nomad.arn
+}
+
+output "blue_target_group_arn" {
+  value = aws_lb_target_group.nomad.arn
+}
+
+output "nomad_endpoint" {
+  value = aws_lb.nomad.dns_name
+}
