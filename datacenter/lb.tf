c1818e4debd29e908e2ffded36a672e550c17f74
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-02-24 15:03:06 -0500

    Update the target group to nomad

--- datacenter/lb.tf
+++ datacenter/lb.tf
@@ -10,28 +10,21 @@ resource "aws_lb" "app" {
   tags = local.tags
 }
 
-resource "aws_lb_target_group" "app" {
-  provider    = aws.datacenter
-  name        = "${var.application_name}-${var.datacenter}"
-  port        = 9090
-  protocol    = "HTTP"
-  vpc_id      = module.vpc.vpc_id
-  target_type = "instance"
+resource "aws_lb_target_group" "nomad" {
+  provider = aws.datacenter
+  name     = "nomad-${var.datacenter}"
+  port     = 4646
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
 
   health_check {
     enabled = true
-    path    = "/health"
+    path    = "/ui"
   }
 }
 
-resource "aws_lb_listener" "app" {
-  provider          = aws.datacenter
-  load_balancer_arn = aws_lb.app.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
+resource "aws_autoscaling_attachment" "nomad" {
+  provider               = aws.datacenter
+  autoscaling_group_name = module.nomad.asg_name_servers
+  alb_target_group_arn   = aws_lb_target_group.nomad.arn
 }
