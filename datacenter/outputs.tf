9730b63b54635f95e3de02ea2b168030a64e0d56
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-02-25 09:43:51 -0500

    Revert "Return outputs of load balancer and target group"
    This reverts commit d112ca465441d9f1ea747798398d549a83126d54.

--- datacenter/outputs.tf
+++ datacenter/outputs.tf
@@ -1,15 +1,3 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
-
-output "listener_arn" {
-  value = aws_lb_listener.nomad.arn
-}
-
-output "blue_target_group_arn" {
-  value = aws_lb_target_group.nomad.arn
-}
-
-output "nomad_endpoint" {
-  value = aws_lb.nomad.dns_name
-}
