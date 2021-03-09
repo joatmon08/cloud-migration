56864d4f993edd58084b6f43eaad659b4e275b7a
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-03-09 14:40:38 -0500

    Output datacenter subnet ids

--- datacenter/outputs.tf
+++ datacenter/outputs.tf
@@ -9,3 +9,11 @@ output "blue_target_group_arn" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
