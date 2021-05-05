29a37406b8a240a364a5a9151e57d31dffae447b
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-05-05 12:59:47 -0400

    Debug consul server

--- datacenter/consul.tf
+++ datacenter/consul.tf
@@ -14,6 +14,7 @@ resource "aws_instance" "consul_server" {
   provider      = aws.datacenter
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name      = "rosemary"
 
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
@@ -24,5 +25,5 @@ resource "aws_instance" "consul_server" {
     dc_public_ip   = aws_eip.consul_server.public_ip
   })
 
-  tags = merge({ "Name" = var.application_name }, local.tags)
+  tags = merge({ "Name" = "consul-server" }, local.tags)
 }
