df386d3c120a79f89adc609445c63957031db958
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-05-10 14:02:49 -0400

    Update security group to use mesh gateway

--- cloud/security-groups.tf
+++ cloud/security-groups.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group_rule" "peered_vpc_ingress" {
   provider          = aws.cloud
   type              = "ingress"
-  from_port         = 30909
-  to_port           = 30909
+  from_port         = 443
+  to_port           = 443
   protocol          = "tcp"
   cidr_blocks       = [data.aws_vpc.datacenter.cidr_block]
   security_group_id = module.eks.cluster_primary_security_group_id
