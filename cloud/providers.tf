7bfbc90a263975e87adf502bb4a9ed9703eaf337
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-05-11 11:13:25 -0400

    Update cloud to assume role for EKS setup

--- cloud/providers.tf
+++ cloud/providers.tf
@@ -20,4 +20,9 @@ provider "aws" {
 provider "aws" {
   alias  = "cloud"
   region = var.region
+  assume_role {
+    role_arn     = var.role_arn
+    session_name = "terraform"
+    external_id  = "terraform"
+  }
 }
